d259d9fa3ae0270f2915fad2da67492f3daae585
emirh <emirh@DESKTOP-T8BGMQF>
2023-03-26 01:56:09 +0300

    while

---
+++ while.py
@@ -0,0 +1,34 @@
+# 1'den 100'e kadar
+
+# x=1
+# while x<=100:
+#     if x%2==0:
+#         print(f'Sayı Çift: {x}')
+#     else:
+#         print(f'Sayı Tek: {x}')
+#     x+=1
+# print('Bitti')
+
+
+
+# x=1
+# y=int(input('Döngünün Bitiş Değerini Giriniz: '))
+# while x<=y:
+#     if x%2==0:
+#         print(f'Sayı Çift: {x}')
+#     else:
+#         print(f'Sayı Tek: {x}')
+#     x+=1
+# print('Bitti')
+
+# name='' # False
+
+# while not name.strip():
+#     name=input('İsminizi Giriniz: ')
+# print(f'Merhaba, {name}')
+
+# sayilar=[1,3,5,7,9,12,19,21]
+# i =0
+# while (i< len(sayilar)):
+#     print(sayilar[i])
+#     i+=1
