2093662928fadd051cc5ef48166d9b90d7ec7add
emirh <emirh@DESKTOP-T8BGMQF>
2023-03-26 18:26:58 +0300

    loop-operators

---
+++ loop_operators.py
@@ -0,0 +1,26 @@
+# Range
+# for item in range(50,100,10):
+#     print(item)
+
+# print(list(range(50,100,10)))
+
+
+# emumerate
+# greeting='Hello'
+
+# for index,item in enumerate(greeting):
+#     print(f'index: {index} letter: {item}')
+
+
+# zip
+# list1=[1,2,3,4,5]
+# list2=['a','b','c','d','e']
+# list3=[100,200,300,400,500]
+
+# print(list(zip(list1,list2,list3)))
+
+# for item in zip(list1,list2,list3):
+#     print(item)
+
+# for a,b,c in zip(list1,list2,list3):
+#     print(b,a,c)
