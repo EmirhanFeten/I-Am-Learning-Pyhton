5347b1de10156cc4ea7b1f971d7ccab9c24c2b7e
emirh <emirh@DESKTOP-T8BGMQF>
2023-03-26 17:57:50 +0300

    while

--- while.py
+++ while.py
@@ -32,3 +32,29 @@
 # while (i< len(sayilar)):
 #     print(sayilar[i])
 #     i+=1
+
+# Kullanıcıdan alınan 5 sayıyı sıralı bir şekilde yazdıran komut satır
+
+# numbers=[]
+# i=0
+# while i<5:
+#     sayi=int(input('Sayı Giriniz: '))
+#     numbers.append(sayi)
+#     i+=1
+# numbers.sort()
+# print(numbers)
+
+urunler=[]
+adet=int(input('Kaç ürün girişi olacak: '))
+i=0
+
+while i<adet:
+    name=input('Ürün Adı: ')
+    price=input('Ürün Fiyatı: ')
+    urunler.append({
+        'name':name,
+        'price':price
+    }) 
+    i+=1
+for urun in urunler:
+    print(f'Urun Adı: {urun["name"]} \n Urun Fiyatı: {urun["price"]}')
