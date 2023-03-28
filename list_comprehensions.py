bc39774ba3a75e3fd2abd933bee18cfb8196e66b
emirh <emirh@DESKTOP-T8BGMQF>
2023-03-29 00:11:08 +0300

    list_comprehension

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -16,5 +16,49 @@
 # result=[x if x%2==0 else "TEK" for x in range(1,10)]
 # print(result)
 
-result=[(x,y) for x in range(3) for y in range(3)]
-print(result)
+# result=[(x,y) for x in range(3) for y in range(3)]
+# print(result)
+
+
+import random
+
+# sayi=random.randint(1,100)
+# hak=5
+# sayac=0
+# puan=100
+# print(sayi)
+# while hak!=0:
+#     sayac+=1
+#     tahmin=int(input("Sayi Tahmin Ediniz: "))
+#     if sayi==tahmin:
+#         print(f"Doğru Tahmin {sayac}. Tahminde bildiniz\n Puanınız {puan}...")
+#         break
+#     else:
+#         print("Yanlış Tahmin")
+#         hak-=1
+#         puan-=20        
+#         print(f'Kalan Puanınız {puan} \nKalan Tahmin Hakkınız {hak}')
+#         if hak==0:
+#             print("Tahmin Hakkınız Bitti")   
+
+# tahmin sayısı kullanıcıdan aldırdık ve tahmin sayısına göre puan yüzdesini hesapladık
+# sayi=random.randint(1,10)
+# can=int(input("Seçim Hak Sayısı Yazınız: "))
+# hak=can
+# sayac=0
+# print(sayi)
+# while hak>0:
+#     hak-=1
+#     sayac+=1
+#     tahmin=int(input("Sayi Tahmin Ediniz: "))
+#     if sayi==tahmin:
+#         print(f"Doğru Tahmin {sayac}. Tahminde bildiniz\n Puanınız {int(100-(100/can)*(sayac-1))}...")
+#         break
+#     elif sayi>tahmin:
+#         print("Yukarı")
+#     else:
+#         print("Aşağı")
+        
+#     if hak==0:
+#         print(f"Tahmin Hakkınız Bitti. Tutulan sayı: {sayi}")   
+
